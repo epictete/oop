867df9e997257bc5e6f1834824b5cd1d20d825a9
epictete <john.laterre@gmail.com>
2020-09-03 14:26:02 +0200

    Helper Class Exercice 2

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
 
 require 'form.php';
+require 'html.php';
 
+$html = new Html();
 $form = new Form($_POST);
+
+echo $html->head();
+echo $html->metaInit();
+echo $html->metaDesc('PHP OOP');
+echo $html->metaKey('PHP, Object Oriented Programming');
+echo $html->css('style');
+echo $html->script('app');
+echo $html->body();
+echo $html->img('php-oop.jpg');
+echo $html->a('https://www.php.net/', 'PHP');
 
 echo $form->formStart('#', 'post');
 echo $form->input('username');
@@ -19,5 +31,7 @@ echo $form->checkbox('mood1', 'happy', 'I am feeling happy!');
 echo $form->checkbox('mood2', 'sad', 'I am feeling sad!');
 echo $form->submit();
 echo $form->formEnd();
+
+echo $html->close();
 
 ?>
